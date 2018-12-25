df5c2d2060303aa595fe8d6e191358c5794e21bf
arctair <jtyler_@outlook.com>
2018-12-25 11:07:15 -0600

    add page not found catcher

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from 'component/Home';
+import NotFound from 'component/NotFound';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route component={NotFound} />
       </Switch>
     );
   }
---
+++ src/component/NotFound.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+import Page from 'component/Page';
+
+class NotFound extends PureComponent {
+  render() {
+    return (
+      <Page>
+        <h2>page not found</h2>
+      </Page>
+    );
+  }
+}
+
+export default NotFound;
